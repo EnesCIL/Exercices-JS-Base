2e9d9a5463839c7daa12a0725863e8f2aae325c7
EnesCIL <enescil2003@hotmail.com>
2025-11-27 14:24:50 +0100

    exercice 2

---
+++ Exercice 2/script.js
@@ -0,0 +1,18 @@
+//prend tout les élement stocké dans carré
+const carres = document.querySelectorAll("#carre div")
+//prend l'element stocké dans affichage
+const affichage = document.querySelector(".affichage")
+//prend l'element stocké dans nomCouleur
+const nomCouleur = document.querySelector(".nomCouleur")
+
+carres.forEach(carre => {
+    carre.addEventListener("click", function () {
+    //recup la couleur du carré cliqué
+    const couleur = getComputedStyle(carre).backgroundColor;
+    console.log(couleur)
+    //permet aplliquer la couleur de fond a l'affichage en prenant la couleur de getComputedstyle qui est cliqué
+    affichage.style.backgroundColor = couleur;
+    //affiche le nom de la couleur au milieu du text en rgb
+    nomCouleur.textContent = couleur;
+  });
+});
